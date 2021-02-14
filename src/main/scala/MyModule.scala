db5d3293badc541f81852d25aa4772e4d39a8868
phosfox <mordredur@googlemail.com>
2021-02-14 22:37:54 +0100

    finishes chap 2

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -4,4 +4,7 @@ object MyModule {
   def main(args: Array[String]): Unit =
     println(factorial(5))
     println(fib(6))
+    println(formatResult("factorial", 7, factorial))
+    println(isSorted(Array(1,2,3,4), (a: Int, b: Int)  => a < b))
+    println(isSorted(Array(), (a: Int, b: Int)  => a > b))
 }
--- src/main/scala/chapter/two/Exercise.scala
+++ src/main/scala/chapter/two/Exercise.scala
@@ -1,6 +1,26 @@
 package chapter.two
 
 object Exercise {
+    def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
+        a => (b => f(a, b))
+    }
+    def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
+        (a, b) => f(a)(b)
+    }
+
+    def compose[A,B,C](f: B => C, g: A => B): A => C = {
+        a => f(g(a))
+    }
+        
+    def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+        def loop(n: Int): Boolean = {
+            if (n >= as.length - 1) true
+            else if (ordered(as(n), as(n+1))) loop(n+1)
+            else false
+        }
+        loop(0)
+    }
+
     def factorial(n: Int): Int = {
         def go(n: Int, acc: Int): Int = 
             if (n <= 0) acc
@@ -15,5 +35,10 @@ object Exercise {
                 case _ => go(n-1, b, a+b)
         }
         go(n, 0, 1)
+    }
+
+    def formatResult(name: String, n: Int, f: Int => Int) = {
+        val msg = "The %s of %d is %d."
+        msg.format(name, n, f(n))
     }
 }
